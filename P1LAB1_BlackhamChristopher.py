d55d17099875549a7a4ccc6391521ce474cc5c49
blackhac0973 <71104519+blackhac0973@users.noreply.github.com>
2020-09-10 18:37:37 -0400

    Add files via upload

---
+++ P1LAB1_BlackhamChristopher.py
@@ -0,0 +1,10 @@
+# fix errors in provided code
+# 9-10-20
+# P1LAB1, zybook 1.12 training basics
+# Chris Blackham
+#
+
+userNum = int(input())
+userNumSquared = userNum * userNum   # fixed bug
+   
+print(userNumSquared)       # issue resolved
